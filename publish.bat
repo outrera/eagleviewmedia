344d3dc6860305848864fe902563ddd38c04ecfc
Jeremy Bullock <jcbullock@gmail.com>
2016-08-20 17:11:00 -0600

    new changes

---
+++ publish.bat
@@ -0,0 +1,9 @@
+git add .
+git commit -m "new changes"
+git push origin master
+xcopy public c:\mycode\eagleviewmedia -Y
+cd c:\mycode\eagleviewmedia
+git add .
+git commit -m "new changes"
+git push origin gh-pages
+cd c:\mycode\talon
