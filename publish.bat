811d4754e61f33eb87f003c889c9306c7553b41b
Jeremy Bullock <jcbullock@gmail.com>
2016-08-20 17:12:57 -0600

    new changes

--- publish.bat
+++ publish.bat
@@ -2,7 +2,7 @@ hugo
 git add .
 git commit -m "new changes"
 git push origin master
-xcopy public c:\mycode\eagleviewmedia /Y
+xcopy public c:\mycode\eagleviewmedia /Y /E
 cd c:\mycode\eagleviewmedia
 git add .
 git commit -m "new changes"
